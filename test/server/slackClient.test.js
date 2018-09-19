76b9fad98fe0a7a59f08e9c55c490b5062ba3db6
Sylum Mastropaolo <sylum.mastropaolo@gmail.com>
2018-09-19 19:46:47 -0400

    Styling

--- test/server/slackClient.test.js
+++ test/server/slackClient.test.js
@@ -8,6 +8,7 @@ describe('slackClient', () => {
     it('should successfully connect to slack', (done) => {
         
         const slackClient = new SlackClient(config.slackToken, config.slackLogLevel, null, null, config.log('test'));
+        
         slackClient.start((slackRes) => {
             slackRes.ok.should.be.true;
             return done();
